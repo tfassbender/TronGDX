01dd4a8cc00cdf8ac87518473f222d4a610d34e7
tobias <tobias-fassbender@gmx.de>
2020-07-10 17:33:45 +0200

    initial commit; basic libGDX project

---
+++ desktop/src/net/jfabricationgames/gdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.jfabricationgames.gdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.jfabricationgames.gdx.TronGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TronGdxGame(), config);
+	}
+}
